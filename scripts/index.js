405f9ca2bc356636b702444cc52325c0bb7184f1
Oussema Chaabouni <oussema.chaabounii@gmail.com>
2021-04-04 19:30:37 +0100

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Jun 03, 2021',
+  name: 'Aladin Weld 7biba'
 };
 
 function hideEverything() {
